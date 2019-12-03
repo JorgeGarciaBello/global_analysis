9f6b5db7d9b3df1713fc7b6eaaedeac077b877ca
JorgeGarciaBello <jogabell@hotmail.com>
2019-12-02 22:22:10 -0600

    Se actualiza el script de compilación

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gfortran   reno_for_global_analysis/reno_subroutines/*.f90 reno_for_global_analysis/*.f90 daya_for_global_analysis/db_subroutines/*.f90   daya_for_global_analysis/*.f90 DDR.f90
+gfortran   reno_for_global_analysis/reno_subroutines/*.f90 reno_for_global_analysis/*.f90 daya_for_global_analysis/db_subroutines/*.f90   daya_for_global_analysis/*.f90 double_chooz_for_global_analysis/dc_subroutines/*.f90 double_chooz_for_global_analysis/*.f90 DDR.f90
 
 #./a.out
 
